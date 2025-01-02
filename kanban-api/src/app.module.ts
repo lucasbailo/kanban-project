cec7b3265ab0518373a8be0846e7178158947e51
Lucas Bailo <lucasebailo@gmail.com>
2025-01-02 14:05:50 -0300

    first commit

---
+++ kanban-api/src/app.module.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        autoLoadEntities: true,
+        synchronize: true
+      }),
+    AuthModule,
+    TodoModule,
+    UserModule
+  ]
+})
+export class AppModule {}
---
+++ kanban-api/src/user/user.module.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { UserService } from './services/user.service';
+import { UserController } from './controllers/user.controller';
+
+@Module({
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
